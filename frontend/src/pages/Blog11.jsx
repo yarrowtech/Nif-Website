51c8379c5dc026eb49d9e18e41c61a11dfdc2105
root <root@srv1094358.hstgr.cloud>
2026-04-10 07:34:26 +0000

    blog11 on vps

--- frontend/src/pages/Blog11.jsx
+++ frontend/src/pages/Blog11.jsx
@@ -1,6 +1,6 @@
 // Blog11.jsx
 import React from "react";
-import heroImg from "../assets/Blog/blog11.png"; // ✅ Make sure to add the actual image at this path
+import heroImg from "../assets/Blog/blog11.jpg"; // ✅ Make sure to add the actual image at this path
 import blog11pic from "../assets/Blog/blog11-pic.png";
 
 export default function Blog11() {
